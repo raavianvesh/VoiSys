2ed6b1d6b0d16ed461edd16b080451c4e1ebdc30
Raavi Anvesh <ranvesh@uccs.edu>
2018-12-18 18:46:51 -0600

    NAudio Library Trial

--- VoiSys/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace VoiSys
-{
-    public class Class1
-    {
-    }
-}
---
+++ VoiSys/QuickStart.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Speech.V1;
+using Google.Cloud.Storage.V1;
+using NAudio;
+using NAudio.Wave;
+using System.Configuration;
+
+
+namespace VoiSys
+{
+    class QuickStart
+    {
+        public static void Main(string[] args)
+        {
+            AuthImplicit(ConfigurationManager.AppSettings.Get("GoogleProjectName"), ConfigurationManager.AppSettings.Get("KeyLocation"));
+            string path = RecordMicroPhone();
+            StreamingRecognizeAsync(path).Wait();
+        }
+
+        static object AuthImplicit(string projectId, string jsonPath)
+        {
+            // If you don't specify credentials when constructing the client, the
+            // client library will look for credentials in the environment.
+            var credential = GoogleCredential.FromFile(jsonPath);
+            var storage = StorageClient.Create(credential);
+            // Make an authenticated API request.
+            var buckets = storage.ListBuckets(projectId);
+            foreach (var bucket in buckets)
+            {
+                Console.WriteLine(bucket.Name);
+            }
+            return null;
+        }
+
+        static string RecordMicroPhone()
+        {
+            string outputfilepath = ConfigurationManager.AppSettings.Get("OutputPath");
+            WasapiLoopbackCapture capture = new WasapiLoopbackCapture();
+            capture.StartRecording();
+            WaveFileWriter writer = new WaveFileWriter(outputfilepath, capture.WaveFormat);
+            capture.DataAvailable += (s, a) =>
+            {
+                writer.Write(a.Buffer, 0, a.BytesRecorded);
+            };
+            capture.RecordingStopped += (s, a) =>
+            {
+                writer.Dispose();
+                writer = null;
+                capture.Dispose();
+            };
+            capture.StopRecording();
+            return outputfilepath;
+        }
+
+        static async Task<object> StreamingRecognizeAsync(string filePath)
+        {
+            var speech = SpeechClient.Create();
+            var streamingCall = speech.StreamingRecognize();
+            // Write the initial request with the config.
+            await streamingCall.WriteAsync(
+                new StreamingRecognizeRequest()
+                {
+                    StreamingConfig = new StreamingRecognitionConfig()
+                    {
+                        Config = new RecognitionConfig()
+                        {
+                            Encoding =
+                            RecognitionConfig.Types.AudioEncoding.Linear16,
+                            SampleRateHertz = 16000,
+                            LanguageCode = "en",
+                        },
+                        InterimResults = true,
+                    }
+                });
+            // Print responses as they arrive.
+            Task printResponses = Task.Run(async () =>
+            {
+                while (await streamingCall.ResponseStream.MoveNext(
+                    default(CancellationToken)))
+                {
+                    foreach (var result in streamingCall.ResponseStream
+                        .Current.Results)
+                    {
+                        foreach (var alternative in result.Alternatives)
+                        {
+                            Console.WriteLine(alternative.Transcript);
+                        }
+                    }
+                }
+            });
+            // Stream the file content to the API.  Write 2 32kb chunks per
+            // second.
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var buffer = new byte[32 * 1024];
+                int bytesRead;
+                while ((bytesRead = await fileStream.ReadAsync(
+                    buffer, 0, buffer.Length)) > 0)
+                {
+                    await streamingCall.WriteAsync(
+                        new StreamingRecognizeRequest()
+                        {
+                            AudioContent = Google.Protobuf.ByteString
+                            .CopyFrom(buffer, 0, bytesRead),
+                        });
+                    await Task.Delay(500);
+                };
+            }
+            await streamingCall.WriteCompleteAsync();
+            await printResponses;
+            return 0;
+        }
+    }
+}
